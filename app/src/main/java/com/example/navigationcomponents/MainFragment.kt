9a7261686fefedcbd1eb52c67a06bc1686af6096
nafiz <naafizrahman1@gmail.com>
2020-06-12 17:10:49 +0600

    Navigation completed with Nav Graph

--- app/src/main/java/com/example/navigationcomponents/MainFragment.kt
+++ app/src/main/java/com/example/navigationcomponents/MainFragment.kt
@@ -29,9 +29,9 @@ class MainFragment : Fragment(),View.OnClickListener {
     }
     override fun onClick(v: View?) {
         when(v!!.id){
-            R.id.view_transactions_btn -> navController.navigate(R.id.action_mainFragment_to_viewTransactionFragment)
-            R.id.view_balance_btn -> navController.navigate(R.id.action_mainFragment_to_viewBalanceFragment)
-            R.id.send_money_btn -> navController.navigate(R.id.action_mainFragment_to_chooseRecipientFragment2)
+            R.id.view_transactions_btn -> navController!!.navigate(R.id.action_mainFragment_to_viewTransactionFragment)
+            R.id.view_balance_btn -> navController!!.navigate(R.id.action_mainFragment_to_viewBalanceFragment)
+            R.id.send_money_btn -> navController!!.navigate(R.id.action_mainFragment_to_chooseRecipientFragment2)
         }
     }
 
